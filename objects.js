5c09ccd9a19db15bad3c963bb862ba5870e7a5c8
Abigail Messmer <abbie.messmer@gmail.com>
2017-10-26 00:26:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -23,3 +23,7 @@ meals.breakfast = 'cereal'
 console.log(meals)
 
 var playlist = { artistName : 'songTitle'};
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist = {artistName : 'songTitle'}; 
+}
